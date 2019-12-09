55442f35dde1e855795970b787fa713cf4e4b8d2
dalejbarr <dalejbarr3@gmail.com>
2019-12-09 14:32:39 +0000

    fit_lmem fit_clmm and power_sim

---
+++ R/fit.R
@@ -0,0 +1,214 @@
+#' @importFrom stats deviance
+#' @importFrom stats logLik
+#' @importFrom stats pchisq
+NULL
+
+tryFit <- function(tf.formula, tf.data, ...) {
+  converged <- TRUE
+  w.handler <- function(w) {
+    converged <- FALSE
+    invokeRestart("muffleWarning")
+  }
+  arg.list <- c(list(formula=tf.formula, data=tf.data), list(...))
+  list(value=withCallingHandlers(tryCatch(
+         do.call(lme4::lmer, arg.list),
+         error=function(e) e),
+         warning=w.handler),
+       converged=converged)
+}
+
+#' Fit linear mixed-effects model to ratings data
+#'
+#' @param .data Data frame, with the format as resulting from a call
+#'   to \code{\link{gen_data}}.
+#' 
+#' @param main_effect Whether to test the main effect of repetition
+#'   (TRUE) or the repetition-by-interval interaction (FALSE; the
+#'   default).
+#'
+#' @details Fits a linear-mixed effects model to the data and tests
+#'   the specified effect (interaction or main effect) using a
+#'   likelihood-ratio test (with lme4 \code{REML = FALSE}).  using
+#'   \code{lme4::lmer()}. If the interaction is to be tested, the
+#'   following two models are compared.
+#'
+#' \code{trating ~ R * (I1 + I2 + I3) +
+#'    (1 + R:I1 + R:I2 + R:I3 || subj_id) +
+#'    (1 + R:I1 + R:I2 + R:I3 || item_id)}
+#'
+#' \code{trating ~ R + I1 + I2 + I3 +
+#'    (1 + R:I1 + R:I2 + R:I3 || subj_id) +
+#'    (1 + R:I1 + R:I2 + R:I3 || item_id)}.
+#'
+#' If the main effect is to be tested, then the following two models
+#' are compared.
+#'
+#' \code{trating ~ R * (I1 + I2 + I3) +
+#'    (1 + R || subj_id) +
+#'    (1 + R || item_id)}
+#'
+#' \code{trating ~ I1 + I2 + I3 + R:I1 + R:I2 + R:I3) +
+#'    (1 + R || subj_id) +
+#'    (1 + R || item_id)}.
+#' 
+#' @return A vector, with the following elements.
+#' \describe{
+#'   \item{\code{(Intercept)}}{Fixed-effects estimate of the intercept.}
+#'   \item{\code{R}}{Fixed-effects estimate of the main effect of repetition.}
+#'   \item{\code{I1}}{Fixed-effects estimate of the main effect of interval (1).}
+#'   \item{\code{I2}}{Fixed-effects estimate of the main effect of interval (2).}
+#'   \item{\code{I3}}{Fixed-effects estimate of the main effect of interval (3).}
+#'   \item{\code{R:I1}}{Fixed-effects estimate of the interaction (1).}
+#'   \item{\code{R:I2}}{Fixed-effects estimate of the interaction (2).}
+#'   \item{\code{R:I3}}{Fixed-effects estimate of the interaction (3).}
+#'   \item{dev1}{Deviance for the model including the effect(s) of interest.}
+#'   \item{dev2}{Deviance for the model excluding the effect(s) of interest.}
+#'   \item{chisq_RI}{Chi-square value for the likelihood ratio test.}
+#'   \item{p_RI}{Associated p-value.}
+#'   \item{m1_singular}{Whether the covariance matrix for model 1 was singular.}
+#'   \item{m2_singular}{Whether the covariance matrix for model 2 was singular.}
+#'   \item{m1_conv}{Whether model 1 converged.}
+#'   \item{m2_conv}{Whether model 2 converged.}
+#' }
+#'
+#' @seealso \code{\link{gen_data}}
+#' @examples
+#' set.seed(62)
+#' dat <- gen_data(40) 
+#' fit_lmem(dat, TRUE) # test main effect
+#' 
+#' @export
+fit_lmem <- function(.data, main_effect = FALSE) {
+  form <- form2 <- NULL
+
+  .data[["trating"]] <- as.integer(.data[["trating"]])
+  
+  if (main_effect) {
+    form <- trating ~ R + I1 + I2 + I3 + R:I1 + R:I2 + R:I3 +
+      (1 + R || subj_id) +
+      (1 + R || item_id)
+    
+    form2 <- trating ~ I1 + I2 + I3 + R:I1 + R:I2 + R:I3 +
+      (1 + R || subj_id) +
+      (1 + R || item_id)
+  } else {
+    form <- trating ~ R * (I1 + I2 + I3) +
+      (1 + R:I1 + R:I2 + R:I3 || subj_id) +
+      (1 + R:I1 + R:I2 + R:I3 || item_id)
+    
+    form2 <- trating ~ R + I1 + I2 + I3 +
+      (1 + R:I1 + R:I2 + R:I3 || subj_id) +
+      (1 + R:I1 + R:I2 + R:I3 || item_id)
+  }
+
+  ## fit the model and print the results
+  el1 <- suppressMessages({tryFit(form, .data, REML = FALSE)})
+  el2 <- suppressMessages({tryFit(form2, .data, REML = FALSE)})
+
+  fef <- lme4::fixef(el1$value)
+
+  mychisq1 <- deviance(el2$value) - deviance(el1$value)
+  pval1 <- pchisq(abs(mychisq1), 3, lower.tail = FALSE)
+
+  c(fef, dev1 = deviance(el1$value), dev2 = deviance(el2$value), 
+    chisq_RI = mychisq1, p_RI = pval1,
+    m1_singular = lme4::isSingular(el1$value),
+    m2_singular = lme4::isSingular(el2$value),
+    m1_conv = el1$converged,
+    m2_conv = el2$converged)
+}
+
+#' Fit cumulative logit mixed-effects model to ratings data
+#'
+#' @inheritParams fit_lmem
+#' 
+#' @details Fits a cumulative logit mixed-effects model to the data
+#'   and tests the specified effect (interaction or main effect) using
+#'   a likelihood-ratio test using \code{ordinal::clmm()}.  If the
+#'   interaction is to be tested, the following two models are
+#'   compared.
+#'
+#' \code{trating ~ R * (I1 + I2 + I3) +
+#'    (1 + R:I1 + R:I2 + R:I3 | subj_id) +
+#'    (1 + R:I1 + R:I2 + R:I3 | item_id)}
+#'
+#' \code{trating ~ R + I1 + I2 + I3 +
+#'    (1 + R:I1 + R:I2 + R:I3 | subj_id) +
+#'    (1 + R:I1 + R:I2 + R:I3 | item_id)}.
+#'
+#' If the main effect is to be tested, then the following two models
+#' are compared.
+#'
+#' \code{trating ~ R * (I1 + I2 + I3) +
+#'    (1 + R | subj_id) +
+#'    (1 + R | item_id)}
+#'
+#' \code{trating ~ I1 + I2 + I3 + R:I1 + R:I2 + R:I3) +
+#'    (1 + R | subj_id) +
+#'    (1 + R | item_id)}.
+#' 
+#' @return A vector, with the following elements.
+#' \describe{
+#'   \item{\code{R}}{Fixed-effects estimate of the main effect of repetition.}
+#'   \item{\code{I1}}{Fixed-effects estimate of the main effect of interval (1).}
+#'   \item{\code{I2}}{Fixed-effects estimate of the main effect of interval (2).}
+#'   \item{\code{I3}}{Fixed-effects estimate of the main effect of interval (3).}
+#'   \item{\code{R:I1}}{Fixed-effects estimate of the interaction (1).}
+#'   \item{\code{R:I2}}{Fixed-effects estimate of the interaction (2).}
+#'   \item{\code{R:I3}}{Fixed-effects estimate of the interaction (3).}
+#'   \item{dev1}{Deviance for the model including the effect(s) of interest.}
+#'   \item{dev2}{Deviance for the model excluding the effect(s) of interest.}
+#'   \item{chisq_RI}{Chi-square value for the likelihood ratio test.}
+#'   \item{p_RI}{Associated p-value.}
+#'   \item{thresh.1|2}{First cut-point (threshold).}
+#'   \item{thresh.2|3}{Second cut-point.}
+#'   \item{thresh.3|4}{Third cut-point.}
+#'   \item{thresh.4|5}{Fourth cut-point.}
+#'   \item{thresh.5|6}{Fifth cut-point.}
+#'   \item{thresh.6|7}{Sixth cut-point.}
+#' }
+#'
+#' @seealso \code{\link{gen_data}}
+#' @examples
+#' \dontrun{
+#'   set.seed(62)
+#'   dat <- gen_data(24) # test main effect
+#'   fit_clmm(dat, TRUE) # takes a few minutes
+#' }
+fit_clmm <- function(.data, main_effect = FALSE) {
+  if (main_effect) {
+    form <- trating ~ R + I1 + I2 + I3 + R:I1 + R:I2 + R:I3 +
+      (1 + R | subj_id) +
+      (1 + R | item_id)
+    
+    form2 <- trating ~ I1 + I2 + I3 + R:I1 + R:I2 + R:I3 +
+      (1 + R | subj_id) +
+      (1 + R | item_id)    
+  } else {
+    form <- trating ~ R * (I1 + I2 + I3) +
+      (1 + R:I1 + R:I2 + R:I3 | subj_id) +
+      (1 + R:I1 + R:I2 + R:I3 | item_id)
+    
+    form2 <- trating ~ R + I1 + I2 + I3 +
+      (1 + R:I1 + R:I2 + R:I3 | subj_id) +
+      (1 + R:I1 + R:I2 + R:I3 | item_id)
+  }
+
+  ## fit the model and print the results
+  el1 <- ordinal::clmm(form, .data, Hess = FALSE)
+  el2 <- ordinal::clmm(form2, .data, Hess = FALSE)
+
+  fef <- el1$beta
+
+  dev1 <- as.numeric(-2 * logLik(el1))
+  dev2 <- as.numeric(-2 * logLik(el2))
+
+  mychisq1 <- dev2 - dev1
+  pval1 <- pchisq(abs(mychisq1), 3, lower.tail = FALSE)
+
+  c(fef,
+    dev1 = dev1,
+    dev2 = dev2, 
+    chisq_RI = mychisq1, p_RI = pval1,
+    thresh = el1$alpha)
+}
---
+++ R/power.R
@@ -0,0 +1,91 @@
+#' Run power simulations
+#'
+#' @param model Which type of model to fit: use 'lmem' for linear
+#'   mixed-effects model, 'clmm' for cumulative logit mixed-effects
+#'   model.
+#'
+#' @param version Which assumption to make about the form of the
+#'   repetition-by-interval interaction; 'asymptote' for the
+#'   asymptotic version, 'single' for the effect-at-a-single-phase
+#'   version (see \code{\link{gen_data}}).
+#'
+#' @param effect Which effect to test, the main effect (\code{'main'})
+#'   or the interaction effect (\code{'interaction'}).
+#' 
+#' @param nsubj Number of subjects.
+#'
+#' @param raw_eff Size of the illusory-truth effect, in raw logit
+#'   units.
+#'
+#' @param nruns How many simulations to run.
+#'
+#' @param outfile Name of output file; \code{NULL} to return the
+#'   simulation results.
+#'
+#' @return Either the name of the outfile (if \code{outfile} is
+#'   non-null) or the results of the simulation (a matrix containing
+#'   results of \code{\link{fit_lmem}} or \code{\link{fit_clmm}}).
+#'
+#' @examples
+#' set.seed(62)
+#' power_sim("lmem", "asymptote", "main", 40, .14, 1, NULL)
+#' \dontrun{
+#' power_sim("clmm", "asymptote", "main", 24, .14, 1, NULL) # takes ~10 minutes
+#' }
+#' @export
+power_sim <- function(model,
+                      version,
+                      effect,
+                      nsubj,
+                      raw_eff,
+                      nruns,
+                      outfile = sprintf("%s_%s_%s_%04d_128_%05d_%0.2f_%s_%d.rds",
+                                        model, version, effect,
+                                        nsubj, nruns,
+                                        raw_eff,
+                                        Sys.info()[["nodename"]],
+                                        Sys.getpid())) {
+  ## make sure the current directory is writeable
+  if (!is.null(outfile)) {
+    writeLines("test", ".zzzxyztest")
+    if (!file.exists(".zzzxyztest"))
+      stop("current directory is not writeable")
+    file.remove(".zzzxyztest")
+
+    if (file.exists(outfile))
+      stop("file '", outfile, "' already exists")
+  }
+
+  if (!(model %in% c("lmem", "clmm")))
+    stop("'model' must be either 'lmem' or 'clmm'")
+
+  if (!(version %in% c("asymptote", "single")))
+    stop("'version' must be either 'asymptote' or 'single'")
+
+  if (!(effect %in% c("main", "interaction")))
+    stop("'effect' must be either 'main' or 'interaction'")
+
+  message("Running ", nruns, " simulations of '", version, "' version.")
+  message("Testing ", effect, " effect, ",
+          "model type ", model, "...")
+  
+  stime <- system.time(results <- replicate(nruns, {
+    dat <- gen_data(nsubj, raw_eff = raw_eff, version = version)
+    do.call(paste0("fit_", model), list(dat))
+  }))
+
+  message(nruns, " simulations completed in ",
+          round(stime[[3]]), " seconds")
+
+  power <- sum(results["p_RI", ] < .05) / nruns
+  message("power for raw effect of ", raw_eff,
+          ": ", power)  
+  
+  if (is.null(outfile)) {
+    results
+  } else {
+    saveRDS(results, outfile)
+    message("Results saved to ", outfile)
+    outfile
+  }
+}
