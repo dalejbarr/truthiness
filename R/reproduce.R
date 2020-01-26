503b61e27ed1ef0a7d80916448169f3ada0c98a7
dalejbarr <dalejbarr3@gmail.com>
2020-01-26 22:42:43 +0000

    oops

--- R/reproduce.R
+++ R/reproduce.R
@@ -6,6 +6,8 @@
 #'
 #' @param outfile Path to the HTML output file.
 #'
+#' @param instructions Include instructions on how to reproduce the analysis.
+#'
 #' @details Runs the R Markdown script on the data in the provided
 #'   subdirectory and renders the HTML report to \code{outfile}. The
 #'   master R Markdown script can be accessed using:
@@ -15,12 +17,20 @@
 #' @export
 reproduce_analysis <- function(path,
                                inferential = FALSE,
-                               outfile = "my_analysis.html") {
+                               outfile = "my_analysis.html",
+                               instructions = FALSE) {
   tf <- tempfile(fileext = ".Rmd")
   doc <- rmarkdown::draft(tf, "illusory-truth-analysis", "truthiness", FALSE, FALSE)
+  if (instructions) {
+    ## overwrite file, including instructions
+    rdoc <- readLines(doc)
+    ix <- grep("^<!-- insert instructions", rdoc)
+    idoc <- readLines(system.file("repr_instr.Rmd", package = "truthiness"))
+    writeLines(c(rdoc[1:(ix - 1L)], idoc, rdoc[(ix + 1L):length(rdoc)]), doc)
+  }
   ofile <- rmarkdown::render(doc, output_file = outfile,
+                             knit_root_dir = getwd(),
                              params = list(subdir = path,
                                            inferential = inferential))
-  message("Successfully wrote report to ", ofile)
   invisible(ofile)
 }
