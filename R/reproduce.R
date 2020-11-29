e59da2e6ec4f19b0da61b7d2ae15ce9f5ffa4549
dalejbarr <dalejbarr@protonmail.com>
2020-11-29 13:45:27 +0000

    update analysis scripts for new prolific format

--- R/reproduce.R
+++ R/reproduce.R
@@ -1,4 +1,4 @@
-#' Reproduce the Master Data Analysis
+#' Reproduce the Master Data Analysis on Raw Data
 #'
 #' @param path Path to the anonymized data files.
 #'
@@ -27,6 +27,48 @@ reproduce_analysis <- function(path,
   if (is.null(infile)) {
     tf <- tempfile(fileext = ".Rmd")
     infile <- rmarkdown::draft(tf, "illusory-truth-analysis", "truthiness",
+                               FALSE, FALSE)
+    message("Processing built-in script against data in '", path, "'")
+  } else {
+    message("Processing '", infile, "' against data in '", path, "'")
+  }
+  ofile <- rmarkdown::render(infile, output_file = basename(outfile),
+                             knit_root_dir = getwd(),
+                             output_dir = dirname(outfile),
+                             params = list(subdir = path,
+                                           recipe = recipe))
+  invisible(ofile)
+}
+
+#' Reproduce the Master Data Analysis on Simulated Data
+#'
+#' @param path Path to the anonymized data files.
+#'
+#' @param outfile Path to the HTML output file.
+#'
+#' @param recipe Include instructions on how to reproduce the analysis.
+#'
+#' @param infile Path to the R Markdown script; \code{NULL} to use the
+#'   built-in script.
+#' 
+#' @details Runs R Markdown script on the data in the provided
+#'   subdirectory and renders the HTML report to \code{outfile}. The
+#'   master R Markdown script can be accessed using:
+#'
+#' \code{rmarkdown::draft("master_script.Rmd",
+#'                        "illusory-truth-analysis-sim", "truthiness")}
+#' 
+#' @export
+reproduce_analysis_sim <- function(path,
+                               outfile = "analysis.html",
+                               recipe = FALSE,
+                               infile = NULL) {
+
+  path <- normalize_path(path)
+  if (!dir.exists(path)) {stop("directory '", path, "' does not exist")}
+  if (is.null(infile)) {
+    tf <- tempfile(fileext = ".Rmd")
+    infile <- rmarkdown::draft(tf, "illusory-truth-analysis-sim", "truthiness",
                                FALSE, FALSE)
     message("Processing built-in script against data in '", path, "'")
   } else {
